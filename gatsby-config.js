883709f44bf4789ed0536a3a9935f9ba3ebd4819
brankeye <brankeye@gmail.com>
2019-10-26 16:43:52 -0400

    Added preconnect to google fonts api.

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,11 @@ module.exports = {
         // Accepts all options defined by `babel-plugin-emotion` plugin.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-preconnect',
+      options: {
+        domains: ['https://fonts.googleapis.com'],
+      },
+    },
   ],
 };
